3ce3ef9485754c455ef1df7a1d7311d1b50cce75
chathura226 <chathuralakshan226@gmail.com>
2024-05-17 14:44:18 +0530

    basic db dunctions with jdbc

--- src/main/java/com/chathuralakshan/springboot3webapp/run/RunRepository.java
+++ src/main/java/com/chathuralakshan/springboot3webapp/run/RunRepository.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -25,6 +26,51 @@ public class RunRepository {
 
     public List<Run> findAll(){
         return jdbcClient.sql("SELECT * FROM run")
+                .query(Run.class)
+                .list();
+    }
+
+    public Optional<Run> findById(Integer id){
+        return jdbcClient.sql("SELECT * FROM run where id=:id limit 1")
+                .param("id",id)
+                .query(Run.class)
+                .optional();
+    }
+
+    public void createRun(Run run){
+        var updated=jdbcClient.sql("INSERT INTO Run(id,title,started_on,completed_on,distance,location) values(?,?,?,?,?,?)")
+                .params(List.of(run.id(),run.title(),run.startedOn(),run.completedOn(),run.distance(),run.location().toString()))
+                .update();
+
+        Assert.state(updated==1,"Failed to create run "+run.title());
+    }
+
+    public void updateRun(Run run,Integer id){
+        var updated=jdbcClient.sql("UPDATE Run SET title=?,started_on=?,completed_on=?,distance=?,location=? where id=?")
+                .params(List.of(run.title(),run.startedOn(),run.completedOn(),run.distance(),run.location().toString(),id))
+                .update();
+
+        Assert.state(updated==1,"Failed to update run "+id);
+    }
+
+
+    public void delete(Integer id){
+        var updated=jdbcClient.sql("DELETE FROM Run where id=:id")
+                .param("id",id)
+                .update();
+
+        Assert.state(updated==1,"Failed to delete run "+id);
+    }
+
+    public int count(){return jdbcClient.sql("SELECT * FROM Run").query().listOfRows().size();}
+
+    public void saveAll(List<Run> runs){
+        runs.stream().forEach(this::createRun);
+    }
+
+    public List<Run> findByLocation(String location){
+        return jdbcClient.sql("SELECT * FROM Run WHERE location=:location")
+                .param("location",location)
                 .query(Run.class)
                 .list();
     }
