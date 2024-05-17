304be28c74607cbb850585435f3d9ecedc28adc2
chathura226 <chathuralakshan226@gmail.com>
2024-05-17 09:50:50 +0530

    first comit

---
+++ src/main/java/com/chathuralakshan/springboot3webapp/SpringBoot3WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.chathuralakshan.springboot3webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3WebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3WebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chathuralakshan/springboot3webapp/SpringBoot3WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chathuralakshan.springboot3webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
